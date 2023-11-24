a5bae5ad608ffd664dff54cd9f1f957fa8d646c5
renee-is-liu <renee.is.liu@gmail.com>
2023-11-23 18:09:24 -0600

    Create a background service worker

--- src/background.ts
+++ src/background.ts
@@ -1,9 +1,10 @@
-chrome.runtime.onInstalled.addListener(() => {
-    chrome.webNavigation.onCompleted.addListener(() => {
-      chrome.tabs.query({ active: true, currentWindow: true }, ([{ id }]) => {
-        if (id) {
-          chrome.action.disable(id);
-        }
-      });
-    }, { url: [{ hostContains: 'google.com' }] });
-  });
+// Example Code: Disable popup for tab containing google domain url
+// chrome.runtime.onInstalled.addListener(() => {
+//     chrome.webNavigation.onCompleted.addListener(() => {
+//       chrome.tabs.query({ active: true, currentWindow: true }, ([{ id }]) => {
+//         if (id) {
+//           chrome.action.disable(id);
+//         }
+//       });
+//     }, { url: [{ hostContains: 'google.com' }] });
+//   });
